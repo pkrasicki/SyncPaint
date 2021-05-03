5f0047d5ad377f219e7e185ce461808475c6bbb6
IngvarX <igorn1995@yandex.ru>
2021-05-03 17:45:01 +0300

    Added circle proper support

--- public/js/tools/ellipse.js
+++ public/js/tools/ellipse.js
@@ -12,16 +12,23 @@ class Ellipse extends Tool
 	draw(ctx, posX, posY, endPosX, endPosY)
 	{
 		ctx.beginPath();
-		let centerX = (posX + endPosX) / 2;
-		let centerY = (posY + endPosY) / 2;
-		let radiusX = Math.abs(centerX - posX);
 
 		if (this.circle)
 		{
-			ctx.arc(posX, posY, radiusX, 0, 2 * Math.PI);
+			let radius = Math.min(Math.abs(endPosX - posX), Math.abs(endPosY - posY)) / 2;
+			let offsetDirectionX = Math.sign(endPosX - posX);
+			let offsetDirectionY = Math.sign(endPosY - posY);
+			let centerX = posX + radius * offsetDirectionX;
+			let centerY = posY + radius * offsetDirectionY;
+
+			ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
 		} else
 		{
+			let centerX = (posX + endPosX) / 2;
+			let centerY = (posY + endPosY) / 2;
+			let radiusX = Math.abs(centerX - posX);
 			let radiusY = Math.abs(centerY - posY);
+
 			ctx.ellipse(centerX, centerY, radiusX, radiusY, 0, 0, 2 * Math.PI);
 		}
 
