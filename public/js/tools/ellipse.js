d15bc70f516d1257d6eb9fc07a68cc4e3f7d3598
pkrasicki <pkrasicki@protonmail.com>
2021-05-03 13:32:25 +0000

    Make the ellipse tool easier to use

--- public/js/tools/ellipse.js
+++ public/js/tools/ellipse.js
@@ -12,10 +12,18 @@ class Ellipse extends Tool
 	draw(ctx, posX, posY, endPosX, endPosY)
 	{
 		ctx.beginPath();
+		let centerX = (posX + endPosX) / 2;
+		let centerY = (posY + endPosY) / 2;
+		let radiusX = Math.abs(centerX - posX);
+
 		if (this.circle)
-			ctx.arc(posX, posY, Math.abs(endPosX - posX), 0, 2 * Math.PI);
-		else
-			ctx.ellipse(posX, posY, Math.abs(endPosX - posX), Math.abs(endPosY - posY), 0, 0, 2 * Math.PI);
+		{
+			ctx.arc(posX, posY, radiusX, 0, 2 * Math.PI);
+		} else
+		{
+			let radiusY = Math.abs(centerY - posY);
+			ctx.ellipse(centerX, centerY, radiusX, radiusY, 0, 0, 2 * Math.PI);
+		}
 
 		ctx.stroke();
 	}
